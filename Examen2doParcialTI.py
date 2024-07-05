21ffbf29790ac553384009e17851bef2f3bc3a70
DESKTOP-U32MFG5\angel <angro1212@gmail.com>
2024-07-04 19:29:08 -0600

    2do Push

--- Examen2doParcialTI.py
+++ Examen2doParcialTI.py
@@ -1,4 +1,4 @@
 # Bienvenido al desarrollo de nuestra aplicación de pijamas
 # En este proyecto, crearemos una aplicación que permitirá a los usuarios explorar y comprar pijamas de alta calidad.
 # Nuestra aplicación ofrecerá una amplia variedad de estilos, tallas y colores para satisfacer las necesidades de todos los usuarios.
-# Además, implementaremos características como filtros de búsqueda, carrito de compras y opciones de pago seguras.
+# Además, implementaremos características como filtros de búsqueda, carrito de compras y opciones de pago seguras..
